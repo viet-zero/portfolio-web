500e54c92791beaddd892be1354ad088d53a4641
viet-zero <viethoangthtv111@gmail.com>
2026-07-25 11:27:59 +0800

    Initial Spring Boot project structure

---
+++ src/main/java/com/portfolio/PortfolioSaasApplication.java
@@ -0,0 +1,12 @@
+package com.portfolio;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioSaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioSaasApplication.class, args);
+	}
+
+}
