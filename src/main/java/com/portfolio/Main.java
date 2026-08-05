694f811abe90f25400c9d5313745f4e76fd04f0c
viet-zero <viethoangthtv111@gmail.com>
2026-08-05 23:43:00 +0800

    thêm và chỉnh sửa UI

--- src/main/java/com/portfolio/Main.java
+++ src/main/java/com/portfolio/Main.java
@@ -1,33 +1,10 @@
 package com.portfolio;
 
-import com.portfolio.entity.Project;
-import com.portfolio.service.ProjectService;
+import com.portfolio.ui.ConsoleUI;
 
 public class Main {
     public static void main(String[] args) {
-        ProjectService projectService = new ProjectService();
-        Project p1 = new Project(1L,
-                "Portfolio",
-                "My personal website",
-                "https://github.com/...",
-                "",
-                "");
-        projectService.addProject(p1);
-        projectService.addProject(new Project(
-            2L,
-            "Coffee Shop",
-            "Quan ly quan ca phe",
-            "github",
-            "",
-            ""
-    ));
-    Project project = projectService.findProjectById(99L);
-
-    if (project != null) {
-        System.out.println(project);
-    } else {
-        System.out.println("Khong tim thay!");
-    }
-        projectService.showProjects();
+        ConsoleUI ui = new ConsoleUI();
+        ui.start();
     }
 }
--- src/main/java/com/portfolio/service/ProjectService.java
+++ src/main/java/com/portfolio/service/ProjectService.java
@@ -6,13 +6,33 @@ import java.util.List;
 public class ProjectService {
     private List<Project> projects = new ArrayList<>();
 
-    public void addProject(Project project){
+    public boolean addProject(Project project){
+
+        if (project.getId() <= 0) {
+            return false;
+        }
+
+        if (project.getTitle() == null ||
+            project.getTitle().isBlank()) {
+            return false;
+        }
+
+        if (project.getDescription() == null ||
+            project.getDescription().isBlank()) {
+            return false;
+        }
+
+        if (findProjectById(project.getId()) != null) {
+            return false;
+        }
+
         projects.add(project);
+        return true;
     }
     public List<Project> getAllProjects(){
         return projects;
     }
-   public void showProjects(){
+    public void showProjects(){
         if (projects.isEmpty()) {
             System.out.println("Danh sách dự án đang trống.");
             return;
@@ -30,4 +50,30 @@ public class ProjectService {
         }
         return null;
     }
+    public boolean removeProjectById(Long Id){
+        Project projectToRemove = findProjectById(Id);
+        if(projectToRemove != null){
+            projects.remove(projectToRemove);
+            return true;
+        }
+        return false;
+    }
+    public boolean updateProject(Long Id,
+                                 String nTitle,
+                                 String nDdesscripTion,
+                                 String nGithubUrl,
+                                 String nDemoUrl,
+                                 String nImageUrl){
+        Project projectUp = findProjectById(Id);
+        if(projectUp != null){
+            projectUp.setTitle(nTitle);
+            projectUp.setDescription(nDdesscripTion);
+            projectUp.setGithubUrl(nGithubUrl);
+            projectUp.setDemoUrl(nDemoUrl);
+            projectUp.setImageUrl(nImageUrl);
+            return true;
+        }                        
+        return false;
+    }
+    
 }
